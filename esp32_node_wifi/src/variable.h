b4e9bce66184a17eeea78c16749eb76d798db087
richard fontaine <ririfonfon10@gmail.com>
2022-11-04 14:58:03 +0100

    Update variable.h

--- esp32_node_wifi/src/variable.h
+++ esp32_node_wifi/src/variable.h
@@ -14,7 +14,7 @@ bool list[MAX_CLIENT] = {false, false, false, false, false, false}; // client
 uint8_t setip1 = 2;
 uint8_t setip2 = 0;
 uint8_t setip3 = 0;
-uint8_t setip4 = 116;
+uint8_t setip4 = 115;
 IPAddress Ip(2, 0, 0, 112);
 
 uint16_t universe_choose = 1;
